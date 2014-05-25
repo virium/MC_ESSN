31390087f108bd0ca57fc8c362e1a69808b584cd
llharry <llharry2@hotmail.co.uk>
2014-05-26 00:33:22 +0100

    Updated

--- MC_OurEssentials/src/io/virium/OE/EssnMain.java
+++ MC_OurEssentials/src/io/virium/OE/EssnMain.java
@@ -1,12 +1,22 @@
 package io.virium.OE;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EssnMain extends JavaPlugin{
+	
+	private static Plugin instance;
+	
+	
 	public void onEnable(){
-		
+		instance = this;
 	}
+	
 	public void onDisable(){
 		
 	}
+	
+    public static Plugin getInstance(){
+    	return instance;
+    }
 }
