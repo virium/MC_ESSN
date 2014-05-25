6183be98638bed85165cb8896348a7e27f95e932
llharry <llharry2@hotmail.co.uk>
2014-05-26 00:29:15 +0100

    desuu

--- MC_OurEssentials/src/io/virium/OE/EssnMain.java
+++ MC_OurEssentials/src/io/virium/OE/EssnMain.java
@@ -1,5 +1,12 @@
 package io.virium.OE;
 
-public class EssnMain {
-sup BITCH
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EssnMain extends JavaPlugin{
+	public void onEnable(){
+		
+	}
+	public void onDisable(){
+		
+	}
 }
